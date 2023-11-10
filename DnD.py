71e95e87d39787eef2a4e216f35e493625425d02
Ariel Hong <arieldhong@gmail.com>
2023-11-10 17:25:35 -0500

    ITEMS ON MAIN!!

--- DnD.py
+++ DnD.py
@@ -18,11 +18,7 @@ def main(item_path, location_path, num_players, end_location):
         
     #do items
     with open(item_path, "r", encoding="utf-8") as f:
-        for line in f:
-            #csv (or json) with item name, price, effect, etc
-            #I guessss we can regex it :vomit:
-            #make a dict of Item objects with the item as the key and the number as the value
-            pass
+        items = json.load(f)
     
     #make game
     with open(location_path, "r", encoding="utf-8") as f:
