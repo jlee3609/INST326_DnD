edf7bb1577b8cc2a5532ed14aa044b10138d2e89
Ariel Hong <arieldhong@gmail.com>
2023-12-04 14:45:01 -0500

    partial items doc string LOLs

--- Item.py
+++ Item.py
@@ -1,20 +1,45 @@
 
 class Item:
     """
+    
+    Attributes:
+        items ():
+        type ():
+        name ():
+        description ():
+        cost ():
+        effects ():
+        quantity ():
     """
     def __init__(self, items, type, name):
+        """ Initializes instance of an Item.
+        
+        Args:
+            items ():
+            type ():
+            name ():
+            
+        Side effects:
+            Sets `items`, `type`, `name`, `description`, `cost`, `effects`, and
+            `quantity` attributes.
+        """
+        self.items = items.copy()
         self.type = type
         self.name = name
-        self.description = items[self.type][self.name]["description"]
-        self.cost = items[self.type][self.name]["cost"]
+        self.description = self.items[self.type][self.name]["description"]
+        self.cost = self.items[self.type][self.name]["cost"]
         #dict within individ item's dict with listed changes to stats
-        self.effects = items[self.type][self.name]["effects"]
-        self.quantity = items[self.type][self.name]["quantity"]
+        self.effects = self.items[self.type][self.name]["effects"]
+        self.quantity = self.items[self.type][self.name]["quantity"]
         
     def __str__(self):
+        """
+        """
         return f"This is a {self.name}: {self.description} Its effects are: {self.effects}"
         
     def stats(self):
+        """
+        """
         for effect in self.effects:
             print(f"Adds {self.effects[effect]} to {effect}")
             
