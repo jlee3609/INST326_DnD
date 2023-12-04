742c6b6229c7f9c38d82da89b63565c164a3bfad
Ariel Hong <arieldhong@gmail.com>
2023-12-04 15:07:43 -0500

    docstring..........

--- Item.py
+++ Item.py
@@ -1,23 +1,30 @@
 
 class Item:
-    """
+    """ Class of an item that a player can buy, own, or use.
     
     Attributes:
-        items ():
-        type ():
-        name ():
-        description ():
-        cost ():
-        effects ():
-        quantity ():
+        items (list of dicts of str:(dict of str:vals): All the items in the game.
+            Each key is the item type; each value is a dict containing the 
+            description, cost, effects (another dict of str:int), and quantity 
+            of each item.
+        type (str): the type of Item, either "potion", "armor", or "weapon".
+        name (str): the name of an Item
+        description (str): the Item's description.
+        cost (int): the cost to purchase Item.
+        effects (dict of str:int): the stat changes. Each key is the Player's stats;
+            each value is an int of the changes each object makes to the Player's stats
+        quantity (int): how many of the item there are in existence.
     """
     def __init__(self, items, type, name):
         """ Initializes instance of an Item.
         
         Args:
-            items ():
-            type ():
-            name ():
+            items (list of dicts of str:(dict of str:vals): All the items in the
+                game. Each key is the item type; each value is a dict containing
+                the description, cost, effects (another dict of str:int), and 
+                quantity of each item.
+            type (str): the type of Item, either "potion", "armor", or "weapon".
+            name (str): the name of an Item.
             
         Side effects:
             Sets `items`, `type`, `name`, `description`, `cost`, `effects`, and
