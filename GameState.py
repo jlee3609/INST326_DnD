72826323f79a56b57cdc9c4284bb799a1363be83
Ariel Hong <arieldhong@gmail.com>
2023-12-11 15:34:43 -0500

    asdf

--- GameState.py
+++ GameState.py
@@ -12,7 +12,7 @@ def generate_npc(gamestate, boss=False):
     
     Args:
         gamestate (GameState): current state of the game that gets altered
-        like items.
+                               like items.
         boss (bool): optional boolean that notes if an NPC is boss-level
             default: False
     
